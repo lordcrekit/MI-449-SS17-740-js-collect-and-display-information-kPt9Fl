22f59f2a51ea50947a294552dd31c41e28ab3aec
William Norman <lordcrekit@gmail.com>
2017-02-15 15:03:11 -0500

    Done.

---
+++ public/js/dah-script.js
@@ -0,0 +1,25 @@
+var profileHtml = document.getElementById('profile-html')
+var profileRaw = document.getElementById('profile-raw')
+
+var firstname = document.getElementsByName('firstname')[0]
+var lastname = document.getElementsByName('lastname')[0]
+var desc = document.getElementsByName('description')[0]
+var email = document.getElementsByName('email')[0]
+var tele = document.getElementsByName('telephone')[0]
+
+function updateProfile() {
+  var content =
+    '<h1>Hi, my name is ' + firstname.value + ' ' + lastname.value + '!</h1>'
+    + '<p>'
+    +    desc.value 
+    + '  <p/>'
+    + '  If you\'re interested in a date, you can email me at '
+    + '  <a href="mailto:' + email.value + '" target="_blank">' + email.value + '</a>'
+    + '  or give me a call at '
+    + '  <a href="tel:' + tele.value + '" target="_blank">' + tele.value + '</a>.'
+    + '</p>'
+  profileHtml.innerHTML = content
+  profileRaw.textContent = content
+}
+
+updateProfile();
